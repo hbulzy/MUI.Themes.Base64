87f4d4244cfd1a123ac1f2a20e72f2a9cb0126ef
charlesmilette <charles.milette@gmail.com>
2014-11-21 11:47:37 -0500

    UI almost ready + :octocat: Added .gitignore
    The UI is waiting that the decoder and encoder are ready
    
    OK button not here
    The decoder/encoder makes me rage
    How to put the file you selected with browse directly after the button, and if not opened, write "Choose a file"?
    How to put a watermark on a textbox?

---
+++ MUI.Themes.Base64/Pages/Decoder.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace MUI.Themes.Base64.Pages
+{
+    /// <summary>
+    /// Interaction logic for Decoder.xaml
+    /// </summary>
+    public partial class Decoder : UserControl
+    {
+        public Decoder()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog Save = new SaveFileDialog();
+
+            Save.Title = "Save as";
+            Save.InitialDirectory = "Desktop";
+            Save.Filter = "Portable Network Graphics|*.png";
+            Save.CheckFileExists = true;
+            Save.CheckPathExists = true;
+            Save.ValidateNames = true;
+            Save.ToString();
+            Save.ShowDialog();
+        }
+    }
+}
---
+++ MUI.Themes.Base64/Pages/Encoder.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace MUI.Themes.Base64.Pages
+{
+    /// <summary>
+    /// Interaction logic for Encoder.xaml
+    /// </summary>
+    public partial class Encoder : UserControl
+    {
+        public Encoder()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog Browse = new OpenFileDialog();
+
+            Browse.Title = "Browse for a file";
+            Browse.InitialDirectory = "Desktop";
+            Browse.Filter = "Portable Network Graphics|*.png";
+            Browse.CheckFileExists = true;
+            Browse.CheckPathExists = true;
+            Browse.ValidateNames = true;
+            Browse.Multiselect = false;
+            Browse.ToString();
+            Browse.ShowDialog();
+        }
+    }
+}
