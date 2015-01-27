3420028092de6ac3a1c99bde077e00bf4e4ce72d
charlesmilette <charles.milette@gmail.com>
2015-01-26 20:18:01 -0500

    Version 1.0.6
    Added image preview and browse function to the decoder

--- MUI.Themes.Base64/Pages/Decoder.xaml.cs
+++ MUI.Themes.Base64/Pages/Decoder.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using Base64Bitmaps;
+using FirstFloor.ModernUI.Windows.Controls;
 using Microsoft.Win32;
 using System;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace MUI.Themes.Base64.Pages
 {
@@ -20,6 +23,12 @@ namespace MUI.Themes.Base64.Pages
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            if (Base64String.Text == string.Empty && BrowsedFile.Text == "Please choose a file")
+            {
+                ModernDialog.ShowMessage("You haven't typed something or choosen a .txt file to convert, duh.", "Dumb!!!", MessageBoxButton.OK);
+            }
+            else
+            {
             SaveFileDialog Save = new SaveFileDialog();
             SavedFile.Text = "   Please choose a file";
             ResultText.BBCode = "The decoding will begin automatically after you select a file.";
@@ -35,16 +44,55 @@ namespace MUI.Themes.Base64.Pages
                 SavedFile.Text = "   "+SaveFile;
                 ResultText.BBCode = "Decoding...";
                 Loading.IsActive = true;
+                string Base64StringToConvert = string.Empty;
+                if (BrowsedFile.Text != "Please choose a file")
+                {
+                    StreamReader streamReader = new StreamReader(BrowsedFile.Text);
+                    Base64StringToConvert = streamReader.ReadToEnd();
+                    streamReader.Close();
+                }
+                else if (Base64String.Text != string.Empty) {Base64StringToConvert = Base64String.Text;}
+                else{}
                 try
                 {
-                    Base64String.Text.Replace("data:image/png;base64,", string.Empty).Base64StringToBitmap().Save(SaveFile, ImageFormat.Png);
-                    ResultText.BBCode = "It has been saved as " + SaveFile + ".";
+                    Base64StringToConvert.Replace("data:image/png;base64,", string.Empty).Base64StringToBitmap().Save(SaveFile, ImageFormat.Png);
+                    ResultText.BBCode = "It has also been saved as " + SaveFile + ".";
                 }
                 catch (Exception exc){ResultText.BBCode = "Good try, maybe another time.";}
-                Base64String.Text = "";
                 Loading.IsActive = false;
-                SavedFile.Text = "   Please choose a file";
+                ResultImage.Source = new BitmapImage(new Uri(SaveFile, UriKind.Absolute));
             }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ResultText.BBCode = "The decoding will begin automatically after you select a file.";
+            Base64String.Text = string.Empty;
+            SavedFile.Text = "   Please choose a file";
+            ResultImage.Source = new BitmapImage(new Uri("Assets/transparent.png", UriKind.Relative));
+            OpenFileDialog Browse = new OpenFileDialog();
+            Browse.Title = "Browse...";
+            Browse.InitialDirectory = "Desktop";
+            Browse.Filter = "Text|*.txt";
+            Browse.CheckFileExists = true;
+            Browse.CheckPathExists = true;
+            Browse.ValidateNames = true;
+            Browse.Multiselect = false;
+            Nullable<bool> FileSelected = Browse.ShowDialog();
+            if (FileSelected == true)
+            {
+                string OpenFile = Browse.FileName;
+                BrowsedFile.Text = OpenFile;
+            }
+        }
+
+        private void Base64String_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            ResultText.BBCode = "The decoding will begin automatically after you select a file.";
+            BrowsedFile.Text = "Please choose a file";
+            SavedFile.Text = "   Please choose a file";
+            ResultImage.Source = new BitmapImage(new Uri("transparent.png", UriKind.Relative));
         }
     }
 }
